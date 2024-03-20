b6bb963f9d24e4df01a46638abc81d13d036a604
Odair <odairodriguez@yahoo.com.br>
2024-03-20 15:26:06 -0300

    feat: modal update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 import Dashboard from "@/components/dashboard";
+import Modal from "@/components/modal";
 import Register from "@/components/register";
+import { GlobalContext } from "@/providers/GlobalContext";
+import { useContext } from "react";
 
 export default function Home() {
+  const { produtoUpdate } = useContext(GlobalContext);
   return (
     <main>
+      {produtoUpdate != null ? <Modal /> : null}
       <Register />
       <Dashboard />
     </main>
---
+++ src/components/modal/index.tsx
@@ -0,0 +1,65 @@
+import { useContext } from "react";
+import styles from "./styles.module.scss";
+import { GlobalContext } from "@/providers/GlobalContext";
+import { useForm } from "react-hook-form";
+
+export default function Modal() {
+  const { produtoUpdate, setProdutoUpdate, updateProduto, getProdutos } =
+    useContext(GlobalContext);
+  const { register, handleSubmit, reset } = useForm();
+
+  async function handleUpdate(data: any) {
+    if (produtoUpdate != null) {
+      const statusOk = await updateProduto(data, produtoUpdate.codigo);
+      if (statusOk) {
+        console.log("Atualização realizada");
+        reset({ descricao: "", preco: "" });
+        getProdutos();
+        setProdutoUpdate(null);
+      } else {
+        // toast.error("Erro ao realizar o registro");
+        console.log("Erro ao realizar a atualização");
+      }
+    }
+    // console.log(data);
+  }
+
+  return (
+    <div className={styles.modal}>
+      {produtoUpdate != null ? (
+        <form
+          className={styles.formProduct}
+          onSubmit={handleSubmit(handleUpdate)}
+        >
+          <h1>Atualizar Produto</h1>
+          <label htmlFor="inpDescription" className={styles.lblDescription}>
+            Descrição:
+          </label>
+          <input
+            type="text"
+            id="inpDescription"
+            required
+            className={styles.description}
+            defaultValue={`${produtoUpdate!.descricao}`}
+            {...register("descricao")}
+          />
+          <label htmlFor="inpPrice" className={styles.lblPrice}>
+            Preço:
+          </label>
+          <input
+            type="number"
+            step="0.01"
+            id="inpPrice"
+            required
+            className={styles.price}
+            defaultValue={Number(`${produtoUpdate!.preco}`)}
+            {...register("preco")}
+          />
+          <button className={styles.btnRegister} type="submit">
+            Atualizar
+          </button>
+        </form>
+      ) : null}
+    </div>
+  );
+}
